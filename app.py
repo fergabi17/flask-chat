88ed8f79c5bc62c92ad36b3193665ae81591542e
Fernanda Cetnarovski <fernanda.cetnarovski@smartbox.com>
2020-07-05 17:19:18 +0100

    Implement basic views and routing

--- app.py
+++ app.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello There</h1>"
+    # Main page with instrictions
+    return "<h1>To send a message use /USER/MESSAGE/</h1>"
+
+
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return f"{username}: {message}"
 
 
 if __name__ =="__main__":
